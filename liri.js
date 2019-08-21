ff3f6f96a6e6660695c1c7b71c0346d87390f58e
Eladio Montenegro <eladio.montenegro@gmail.com>
2019-08-20 22:10:39 -0500

    added switch case code and did seudo coding for the rest of the app

--- liri.js
+++ liri.js
@@ -1,15 +1,66 @@
 require("dotenv").config();
 
+var axios = require("axios");
+
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
+//switch statement functions
+function UserInputs (userOptions, inputParameter){
+    switch (userOption) {
+    case 'concert-this':
+        showConcertInfo(inputParameter);
+        break;
+    case 'spotify-this-song':
+        break;
+    case 'movie-this':
+        showMovieInfo(inputParameter);
+        break;
+    case 'do-what-it-says':
+        showSomeInfo();
+        break;
+
+    }
+}
+//function for concert info
+
+
+//function for music info: spotify
 
 
 
+//function for movie info: OMDB
 
-// commands the app needs to take in 
-// concert-this
-// spotify-this-song
-// movie-this
+// Store all of the arguments in an array
+var nodeArgs = process.argv;
+
+// Create an empty variable for holding the movie name
+var movieName = "";
+
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  } else {
+    movieName += nodeArgs[i];
+
+  }
+}
+
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// This line is just to help us debug against the actual URL.
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+  function(response) {
+    console.log("Release Year: " + response.data.Year);
+  })
 // do-what-it-says
+
+
+//function fo reading out of random.txt file
